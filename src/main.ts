0aeace66ec1235650dee33e266f38b32446442d6
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-15 20:52:18 -0300

    Created getCtx function

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 // import SpaceShip from './models/SpaceShip'
 
 let canvas: HTMLCanvasElement | null
+let ctx: CanvasRenderingContext2D
 // let playerShip: SpaceShip
 
 (function init(): void {
     canvas = initCanvas()
     console.log(canvas)
+    ctx = getCtx(canvas)
 })()
 
 function initCanvas(): HTMLCanvasElement {
@@ -17,4 +19,13 @@ function initCanvas(): HTMLCanvasElement {
     canvasFound.height = window.innerHeight
 
     return canvasFound
+}
+
+function getCtx(canvas: HTMLCanvasElement): CanvasRenderingContext2D {
+    var ctxFound = canvas.getContext('2d');
+    if (!ctxFound || !(ctxFound instanceof CanvasRenderingContext2D)) {
+        throw new Error('Falha ao adquirir o contexto 2D');
+    }
+
+    return ctxFound
 }
