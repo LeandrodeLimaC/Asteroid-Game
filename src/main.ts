50fb75368e055a1f377d916ff2bee587f3342b31
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-15 20:51:52 -0300

    Created canvas init function

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
-import SpaceShip from './models/SpaceShip'
+// import SpaceShip from './models/SpaceShip'
 
-let canvas: HTMLElement
-let playerShip: SpaceShip
+let canvas: HTMLCanvasElement | null
+// let playerShip: SpaceShip
 
 (function init(): void {
-    canvas = document.getElementById('asteroids-game')!;
-    playerShip = new SpaceShip(canvas)
-
-    playerShip.update()
+    canvas = initCanvas()
+    console.log(canvas)
 })()
+
+function initCanvas(): HTMLCanvasElement {
+    const canvasFound = document.querySelector('#asteroids-game');
+    if (!canvasFound || !(canvasFound instanceof HTMLCanvasElement))
+        throw new Error('Falha ao encontrar canvas')
+
+    canvasFound.width = window.innerWidth
+    canvasFound.height = window.innerHeight
+
+    return canvasFound
+}
