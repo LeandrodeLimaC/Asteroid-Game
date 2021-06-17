4f9999d3c0c451db34d683abec8944c7ad4f70e6
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-16 22:25:22 -0300

    Added initial attributes to class

--- src/models/SpaceShip.ts
+++ src/models/SpaceShip.ts
@@ -1,14 +1,25 @@
+// Shared types
+type Vector = {
+    x: number;
+    y: number;
+};
 export class SpaceShip {
+    private speed: number = 0.1;
+    private velocity: Vector = { x: 0, y: 0 };
+    private nosePos: Vector = { x: 0, y: 0 };
+    private rotateSpeed = 0.001;
+    private movingForward = false;
+    private radius: number = 15;
+
     constructor(
-        protected canvas: HTMLElement,
-        protected yPosition: number = 0,
-        protected xPosition: number = 0,
+        protected position: Vector,
+        public angle: number = 0
     ) { }
 
     update(): void {
         throw new Error("Method update not implemented.");
     }
     draw(): void {
-        throw new Error("Method not implemented.");
+        throw new Error("Method draw not implemented.");
     }
 }
