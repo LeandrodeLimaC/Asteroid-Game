f5b3729483a69abc8cce3692f4d969d2cd930f16
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-07-12 23:42:38 -0300

    Position made public

--- src/models/Object2D.ts
+++ src/models/Object2D.ts
@@ -1,7 +1,7 @@
 import { Vector } from '../types'
 
 export abstract class Object2D {
-  protected position: Vector = { x: 0, y: 0 }
+  public position: Vector = { x: 0, y: 0 }
 
   abstract update(): void;
   abstract draw(context: CanvasRenderingContext2D): void;
