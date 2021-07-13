85d065a247e7a7ebb4121dcd5be3e93a96b6b9c7
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-07-12 23:41:29 -0300

    Bullet class

--- src/models/Bullet.ts
+++ src/models/Bullet.ts
@@ -1,1 +1,38 @@
-export class Bullet { }
+import { Object2D } from "./Object2D";
+import { SpaceShip } from "./SpaceShip";
+
+import { toRadians } from "../utils";
+
+export class Bullet extends Object2D {
+  private _acceleration: number = 5;
+
+  public collisionRadius: number = 3;
+  private _height: number = 3;
+  private _width: number = 3;
+
+  constructor(
+    private spaceShip: SpaceShip,
+    private angle: number
+  ) {
+    super()
+
+    this.position = {
+      x: this.spaceShip.weaponPosition.x,
+      y: this.spaceShip.weaponPosition.y
+    }
+  }
+
+  update(): void {
+    let radians = toRadians(this.angle)
+    this.position.x -= Math.cos(radians) * this._acceleration;
+    this.position.y -= Math.sin(radians) * this._acceleration;
+    // console.log(Math.cos(radians) * this._acceleration, Math.sin(radians) * this._acceleration)
+  }
+  draw(context: CanvasRenderingContext2D): void {
+    // console.log("draw bullets")
+    this.update()
+    context.fillStyle = 'white';
+    // console.log(this.position.x, this.position.y, this._width, this._height)
+    context.fillRect(this.position.x, this.position.y, this._width, this._height)
+  }
+}
